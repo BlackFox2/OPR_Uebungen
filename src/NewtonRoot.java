d836b6d72de3ee535a3c0d5149a428b4e73901a8
BlackFox2 <ice-cold@hotmail.de>
2015-04-12 22:06:47 +0200

    Added comments

--- src/NewtonRoot.java
+++ src/NewtonRoot.java
@@ -1,44 +1,69 @@
 import java.util.Scanner;
 
 /**
- * Created by blackfox on 01.04.15.
+ * Program to calculate an approximation to the square and cubic root with the Newton-Method.
+ *
+ * @author Markus Marihart
+ * @version 1.0
  */
 public class NewtonRoot {
 
-
+    /**
+     * Asks for a number > 0 to calculate the square and cubic root.
+     *
+     * @param args console arguments (not used)
+     */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         double n;
         double sRoot , cRoot;
-        System.out.print("Bitte geben Sie eine Zahl ein: ");
+        NewtonRoot nR = new NewtonRoot();
+        System.out.print("Bitte geben Sie eine Zahl groesser 0 ein: ");
         n = s.nextDouble();
-        sRoot = squareRoot(n);
+        if(!(n > 0.0)) {
+            System.out.println("Die Zahl muss groesser 0 sein!");
+            System.exit(-1);
+        }
+        sRoot = nR.squareRoot(n);
         System.out.printf("Die Quadratwurzel von %e lautet %.15f\n", n, sRoot);
-        cRoot = cubicRoot(n);
+        cRoot = nR.cubicRoot(n);
         System.out.printf("Die Kubikwurzel von %e lautet %.15f\n", n, cRoot);
     }
 
-    public static double squareRoot(double n) {
-        double target_diff = 0.000001;
+    /**
+     * Function to calculate a approximation to the the square root for a given number with a maximum deviation of
+     * 0.000001
+     *
+     * @param n the number of which to calculate the approximation to the square root
+     * @return the approximation to the square root of the given value
+     */
+    public double squareRoot(double n) {
+        double target_diff = 0.000001;                      //maximum deviation
         double q = 1.0;
-        double act_diff;
+        double act_diff;                                    //actual deviation
 
         do {
-            act_diff = Math.abs(n-q*q);
-            q = (q+(n/q))/2;
+            act_diff = Math.abs(n-q*q);                     //update the actual deviation
+            q = (q+(n/q))/2;                                //calculate new q value
         } while(act_diff > target_diff);
 
         return q;
     }
 
-    public static double cubicRoot(double n) {
-        double target_diff = 0.000001;
+    /**
+     * Function to calculate a approximation to the the square root for a given number with a maximum deviation of
+     * 0.000001
+     *
+     * @param n the number of which to calculate the approximation to the cubic root
+     * @return the approximation to the cubic root of the given value
+     */
+    public double cubicRoot(double n) {
+        double target_diff = 0.000001;                      //maximum deviation
         double q = 1.0;
-        double act_diff;
+        double act_diff;                                    //actual deviation
         do {
-            act_diff = Math.abs(n-q*q*q);
-            //q = 2/3*q + n/(3*q*q);
-            q = (2*q+n/(q*q))/3;
+            act_diff = Math.abs(n-q*q*q);                   //update the actual deviation
+            q = (2*q+n/(q*q))/3;                            //calculate new q value
         } while(act_diff > target_diff);
         return q;
     }
--- src/NewtonRootTest.java
+++ src/NewtonRootTest.java
@@ -3,50 +3,61 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Created by @Author Markus Marihart
- * @version 1.0
+ * JUnitTest for testing the NewtonRoot class and it's functionality.
  *
+ * @author Markus Marihart
+ * @version 1.0
  */
 public class NewtonRootTest {
 
+    /**
+     * Checks the squareRoot function from NewtonRoot class with different inputs.
+     * @throws Exception
+     */
     @Test
     public void testSquareRoot() throws Exception {
         double n = 49;
         double result;
-        result = NewtonRoot.squareRoot(n);
+        NewtonRoot nR = new NewtonRoot();
+        result = nR.squareRoot(n);
         assertEquals(Math.sqrt(n), result, 0.000001);
 
         n = 0.5;
-        result = NewtonRoot.squareRoot(n);
+        result = nR.squareRoot(n);
         assertEquals(Math.sqrt(n), result, 0.000001);
 
         n = 0.1;
-        result = NewtonRoot.squareRoot(n);
+        result = nR.squareRoot(n);
         assertEquals(Math.sqrt(n), result, 0.000001);
 
         n = 0.0005;
-        result = NewtonRoot.squareRoot(n);
+        result = nR.squareRoot(n);
         assertEquals(Math.sqrt(n), result, 0.000001);
 
     }
 
+    /**
+     * Checks the cubicRoot function from NewtonRoot class with different inputs.
+     * @throws Exception
+     */
     @Test
     public void testCubicRoot() throws Exception {
         double n = 34;
         double result;
-        result = NewtonRoot.cubicRoot(n);
+        NewtonRoot nR = new NewtonRoot();
+        result = nR.cubicRoot(n);
         assertEquals(Math.cbrt(n),result, 0.000001);
 
         n = 0.5;
-        result = NewtonRoot.cubicRoot(n);
+        result = nR.cubicRoot(n);
         assertEquals(Math.cbrt(n), result, 0.000001);
 
         n = 0.1;
-        result = NewtonRoot.cubicRoot(n);
+        result = nR.cubicRoot(n);
         assertEquals(Math.cbrt(n), result, 0.000001);
 
         n = 0.0005;
-        result = NewtonRoot.cubicRoot(n);
+        result = nR.cubicRoot(n);
         assertEquals(Math.cbrt(n), result, 0.000001);
     }
 }
