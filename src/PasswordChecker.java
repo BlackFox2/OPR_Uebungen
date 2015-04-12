83498da6c10df168a49d0d287bde1134a63221e3
BlackFox2 <ice-cold@hotmail.de>
2015-04-12 21:36:32 +0200

    Added comments

--- src/PasswordChecker.java
+++ src/PasswordChecker.java
@@ -1,20 +1,50 @@
 /**
- * Created by blackfox on 31.03.15.
+ * A program to check one or more given passwords for important attributes regarding the password strength.
+ * These attributes are the length, capital/lower case letters, numbers, special characters and combinations of them.
+ *
+ * @author Markus Marihart
+ * @version 1.0
  */
 public class PasswordChecker {
+    /**
+     * Boolean whether or not the password is long enough (8 characters in our case)
+     */
     boolean isLongEnough = false;
+    /**
+     * Boolean whether or not the password contains at least two numbers and two letters
+     */
     boolean hasTwoNumsLets = false;
+    /**
+     * Boolean whether or not the password has at least one small letter
+     */
     boolean hasSmallLet = false;
+    /**
+     * Boolean whether or not the password has at least one big letter
+     */
     boolean hasBigLet = false;
+    /**
+     * Boolean whether or not the password contains at least one special character
+     */
     boolean hasSpecialChar = false;
+    /**
+     * Boolean whether or not the password contains at least one special character and one number
+     */
     boolean oneNumSpecChar = false;
+    /**
+     * The password to check
+     */
     String pw;
 
-    // (.) any character; * zero or more times
+    /**
+     *  Regex to check if a special character is in the password
+     *  (.) any character; * zero or more times
+     */
+
     final String SPECIALREGEX = "(.)*[!\"#$%&'()*+,-./:;<=>?@](.)*";
 
+
     public static void main(String[] args) {
-        System.out.println(args.length);
+
         if (args.length < 1) {
             System.out.println("Usage: <password to check>");
             System.exit(0);
@@ -24,20 +54,30 @@ public class PasswordChecker {
         for (String x : args) {
             pwc.reset();
             pwc.setPw(x);
-            pwc.checkLength();
-            pwc.checkNumsLet();
+            pwc.isLongEnough = pwc.checkLength();
+            pwc.hasTwoNumsLets = pwc.checkNumsLet();
             pwc.checkSmallCapitalLet();
-            pwc.checkSpecialChar();
-            pwc.checkNumOrSpecChar();
+            pwc.hasSpecialChar = pwc.checkSpecialChar(pwc.SPECIALREGEX);
+            pwc.oneNumSpecChar = pwc.checkNumOrSpecChar();
             pwc.printResult();
+
         }
 
     }
 
+
+    /**
+     * Set the password to check
+     *
+     * @param pw Password to check
+     */
     public void setPw(String pw) {
         this.pw = pw;
     }
 
+    /**
+     * Reset all values in the class for a new check
+     */
     public void reset() {
         isLongEnough = false;
         hasTwoNumsLets = false;
@@ -45,18 +85,28 @@ public class PasswordChecker {
         hasBigLet = false;
         hasSpecialChar = false;
         oneNumSpecChar = false;
+        pw = "";
     }
 
-    private void checkLength() {
-        if (pw.length() >= 8) {
-            isLongEnough = true;
-        }
+    /**
+     * Check the length of the given password.
+     *
+     * @return  Boolean whether or not the password is long enough
+     */
+    private boolean checkLength() {
+        return pw.length() >= 8;
     }
 
-    private void checkNumsLet() {
+    /**
+     * Check whether or not the password contains at least two numbers and two letters.
+     *
+     * @return Whether or not the password fulfills the requirements.
+     */
+    private boolean checkNumsLet() {
         int letterCount = 0;
         int numCount = 0;
         for (int i = 0; i < pw.length(); i++) {
+            //if we have already found 2 letters we don't need to look for them.
             if (letterCount < 2 && (pw.charAt(i) >= 'A' && pw.charAt(i) <= 'Z'
                     || pw.charAt(i) >= 'a' && pw.charAt(i) <= 'z')) {
                 letterCount++;
@@ -64,36 +114,49 @@ public class PasswordChecker {
                 numCount++;
             }
             if (numCount >= 2 && letterCount >= 2) {
-                hasTwoNumsLets = true;
-                break;
+                return true;
             }
-
-
         }
+        return false;
     }
 
+    /**
+     * Checks if the password contains at least one small and one capital letter.
+     * Check is performed with regex classes
+     */
     private void checkSmallCapitalLet() {
-        if (pw.matches("(.)*[\\p{Lower}](.)*")) {
+        if (pw.matches("(.)*[\\p{javaLowerCase}](.)*")) {   // [\\p{Lower}] POSIX character class
             hasSmallLet = true;
         }
-        if (pw.matches("(.)*[\\p{Upper}](.)*")) {
+        if (pw.matches("(.)*[\\p{Upper}](.)*")) {   // [\\p{Upper}] POSIX character class
             hasBigLet = true;
         }
     }
 
-    private void checkSpecialChar() {
-        if (pw.matches(SPECIALREGEX)) {
-            hasSpecialChar = true;
-        }
+    /**
+     * Checks if the password contains a special character.
+     *
+     * @param regex Regex string with all allowed special characters
+     * @return true when one of the characters is in the password
+     */
+    private boolean checkSpecialChar(String regex) {
+        return pw.matches(SPECIALREGEX);
     }
 
-    private void checkNumOrSpecChar() {
-        checkSpecialChar();
-        if (hasSpecialChar || pw.matches("(.)*[\\d](.)*")) {
-            oneNumSpecChar = true;
-        }
+    /**
+     * Checks if the password contains at least one number and one special character.
+     * Method checkSpecialChar is used to determine if a special character is in the password.
+     *
+     * @return true if the password includes at least one number and special character
+     */
+    private boolean checkNumOrSpecChar() {
+        checkSpecialChar(this.SPECIALREGEX);
+        return hasSpecialChar || pw.matches("(.)*[\\d](.)*");   // [\\d] Regex digit-class
     }
 
+    /**
+     * Prints the result of all checks with further information which requirement the password doesn't fulfill
+     */
     private void printResult() {
         System.out.println("You entered the following password: " + pw);
         if (!isLongEnough) {
