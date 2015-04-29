894fc431279501a4c34801051ba0568a74878cad
Markus Marihart <herp@derp.com>
2015-04-29 13:07:40 +0200

    Finished UnitTest for TriangleClass

--- src/uebung2/TriangleTest.java
+++ src/uebung2/TriangleTest.java
@@ -63,11 +63,62 @@ public class TriangleTest {
         double movedX = 10;
         double movedY = 10;
         Triangle t1 = t.moved(movedX, movedY);
-        //TODO finish this test and testZoomed
+
+        for(int i  = 0; i < t.points.length; i++) {
+            assertEquals(t.points[i].getX() + movedX, t1.points[i].getX(), 0.0);
+            assertEquals(t.points[i].getY() + movedY, t1.points[i].getY(), 0.0);
+        }
+
+        movedX = -5;
+        movedY = -39.6;
+        t1 = t.moved(movedX, movedY);
+
+        for(int i  = 0; i < t.points.length; i++) {
+            assertEquals(t.points[i].getX() + movedX, t1.points[i].getX(), 0.0);
+            assertEquals(t.points[i].getY() + movedY, t1.points[i].getY(), 0.0);
+        }
+
+        movedX = 0.0;
+        movedY = 0.0;
+        t1 = t.moved(movedX, movedY);
+
+        for(int i  = 0; i < t.points.length; i++) {
+            assertEquals(t.points[i].getX() + movedX, t1.points[i].getX(), 0.0);
+            assertEquals(t.points[i].getY() + movedY, t1.points[i].getY(), 0.0);
+        }
     }
 
     @Test
     public void testZoomed() throws Exception {
+        Triangle t = new Triangle(new Point(1,1), new Point(3,1), new Point(3,2));
+        double f = 3.0;
+        Triangle t1 = t.zoomed(f);
+        Point center = new Point(0,0);
+
+        double[] distancesOrig = new double[3];
+        distancesOrig[0] = t.points[0].distance(center);
+        distancesOrig[1] = t.points[1].distance(center);
+        distancesOrig[2] = t.points[2].distance(center);
+
+        double [] distancesZoom = new double[3];
+        distancesZoom[0] = t1.points[0].distance(center);
+        distancesZoom[1] = t1.points[1].distance(center);
+        distancesZoom[2] = t1.points[2].distance(center);
+
+        assertEquals(distancesOrig[0]*Math.sqrt(f*f), distancesZoom[0], 0.000000000001);
+        assertEquals(distancesOrig[1]*Math.sqrt(f*f), distancesZoom[1], 0.000000000001);
+        assertEquals(distancesOrig[2]*Math.sqrt(f*f), distancesZoom[2], 0.000000000001);
+
+        f = -0.5;
+        t1 = t.zoomed(f);
+
+        distancesZoom[0] = t1.points[0].distance(center);
+        distancesZoom[1] = t1.points[1].distance(center);
+        distancesZoom[2] = t1.points[2].distance(center);
+
+        assertEquals(distancesOrig[0]*Math.sqrt(f*f), distancesZoom[0], 0.000000000001);
+        assertEquals(distancesOrig[1]*Math.sqrt(f*f), distancesZoom[1], 0.000000000001);
+        assertEquals(distancesOrig[2]*Math.sqrt(f*f), distancesZoom[2], 0.000000000001);
 
     }
 }
