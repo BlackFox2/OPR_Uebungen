82da44a0c4e5965de0db0035f2500bef98766b6b
Markus Marihart <herp@derp.com>
2015-05-04 15:10:53 +0200

    Added Interface Deque

---
+++ src/uebung2/Deque.java
@@ -0,0 +1,13 @@
+package uebung2;
+
+/**
+ * Created by @author
+ */
+public interface Deque {
+    void addHead();
+    void addTail();
+    void removeHead();
+    void removeTail();
+    Node getHead();
+    Node getTail();
+}
