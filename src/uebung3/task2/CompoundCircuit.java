42fc94f4bc49b11a1192a773e9968ba0853cc1d7
Markus Marihart <herp@derp.com>
2015-05-25 12:40:02 +0200

    Implemented class CompoundCircuit

---
+++ src/uebung3/task2/CompoundCircuit.java
@@ -0,0 +1,15 @@
+package uebung3.task2;
+
+/**
+ * Created by @author
+ */
+public class CompoundCircuit {
+
+    Circuit c1;
+    Circuit c2;
+
+    public CompoundCircuit(Circuit c1, Circuit c2) {
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+}
