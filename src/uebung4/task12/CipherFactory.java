207e511df30a69a47e83943fab194facb7e5cf42
Markus Marihart <herp@derp.com>
2015-06-11 19:59:04 +0200

    Implemented class CipherFactory

---
+++ src/uebung4/task12/CipherFactory.java
@@ -0,0 +1,38 @@
+package uebung4.task12;
+
+/**
+ * This class is a factory for encryption algorithms.
+ *
+ * @author Markus Marihart
+ * @version 1.0
+ */
+public class CipherFactory {
+
+    /**
+     * Creates and returns an object of the given algorithm name.
+     *
+     * @param name      Encryption algorithm to create object for
+     * @return          Object for the specified algorithm
+     * @throws IllegalArgumentException     when name does not match an available algorithm
+     */
+    public static Cipher getInstance(String name) throws IllegalArgumentException {
+        switch (name) {
+            case "XORSubstitution":
+                return new XorSub();
+            case "StreamCipher":
+                return new StreamCipher();
+            case "AdditiveSubstition":
+                return new AddSub();
+            default:
+                throw new IllegalArgumentException(name + " is not a valid algorithm!");
+        }
+    }
+
+    /**
+     * Creates and returns an AddSub object.
+     * @return  AddSub object.
+     */
+    public static Cipher getInstance() {
+        return new AddSub();
+    }
+}
