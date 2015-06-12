c809650d279398938d29c6cdf92b022fad709777
Markus Marihart <herp@derp.com>
2015-06-12 10:56:06 +0200

    changed implementation to use Template-Method-Pattern. Therefore added method doCrypto

--- src/uebung4/task12/FileDecrypter.java
+++ src/uebung4/task12/FileDecrypter.java
@@ -8,7 +8,7 @@ import java.io.*;
  * @author Markus Marihart
  * @version 1.0
  */
-public class FileDecrypter {
+public class FileDecrypter extends FileCryptoHandler{
     private int key;
     private final String decExt = "dec";
     private Cipher cipher;
@@ -18,9 +18,9 @@ public class FileDecrypter {
      * @param key       key with which the decryption works
      * @param cipher    decryption method
      */
-    public FileDecrypter(int key, Cipher cipher) {
+    public FileDecrypter(int key, String cipher) {
         this.key = key;
-        this.cipher = cipher;
+        this.cipher = CipherFactory.getInstance(cipher);
     }
 
     /**
@@ -28,7 +28,7 @@ public class FileDecrypter {
      * @param key   key with which the decryption works
      */
     public FileDecrypter(int key) {
-        this(key, CipherFactory.getInstance());
+        this(key, CipherFactory.defaultCipher);
     }
 
     /**
@@ -39,45 +39,14 @@ public class FileDecrypter {
      * @param inputPath     Path to file with .enc extension
      */
     public void decrypt(final String inputPath) {
-        String ext = "";
-        int i  = inputPath.lastIndexOf('.');
-        if(i > 0) {
-            ext = inputPath.substring(i+1);
-        }
-        if(ext.equals("enc")) {
-            File f = new File(inputPath);
-            if (!f.exists()) {
-                System.out.println("Error: File does not exist!");
-                return;
-            }
-            FileInputStream in;
-            try {
-                in = new FileInputStream(f);
-            } catch (FileNotFoundException e) {
-                System.out.println("Error: File does not exist!");
-                return;
-            }
-            byte input[] = new byte[(int)f.length()];
-            ext = ext.replace("enc", decExt);
-            String newPath = inputPath.substring(0, i);
-            newPath += ext;
-            FileOutputStream out;
-            try {
-                out = new FileOutputStream(newPath);
-                in.read(input);
-            } catch (FileNotFoundException e) {
-                System.out.println("Error: Not able to create output file!");
-                return;
-            } catch (IOException e) {
-                System.out.println("Error: Not able to read input file!");
-                return;
-            }
-            try {
-                out.write(cipher.decrypt(input, key));
-            } catch (IOException e) {
-                System.out.println("Error: Not able to write to output file!");
-                return;
-            }
-        }
+        fileHandling(inputPath);
+    }
+
+    /**
+     * @see FileCryptoHandler
+     */
+    @Override
+    protected byte[] doCrypto(byte[] input) {
+        return cipher.decrypt(input, key);
     }
 }
